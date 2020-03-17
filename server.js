37390a1759fca791e766989b6f9670a7d083040b
Gil Solis <gilsolisuw@gmail.com>
2020-03-16 22:49:42 -0700

    adding switch in response

--- server.js
+++ server.js
@@ -16,18 +16,40 @@ connection.connect(function(err) {
 
 function start() {
   console.log("inside the start()");
-  inquirer.prompt([
-    {
-      name: "action",
-      type: "list",
-      message: "what would you like to do?",
-      choices: ["Add", "View", "Update", "Delete"]
-    },
-    {
-      name: "option",
-      type: "list",
-      message: "Select from these options?",
-      choices: ["Employee", "Role", "Department"]
-    }
-  ]);
+  inquirer
+    .prompt([
+      {
+        name: "action",
+        type: "list",
+        message: "what would you like to do?",
+        choices: ["Add", "View", "Update", "Delete"]
+      },
+      {
+        name: "option",
+        type: "list",
+        message: "Select from these options?",
+        choices: ["Employee", "Role", "Department"]
+      }
+    ])
+    .then(function(res) {
+      console.log(`You have chosen to ${res.action} ${res.option}`);
+
+      switch (res.action) {
+        case "Add":
+          createData(res.option);
+          break;
+        case "View":
+          readData(res.option);
+          break;
+        case "Update":
+          updateData(res.option);
+          break;
+        case "Delete":
+          deleteData(res.option);
+          break;
+      }
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
