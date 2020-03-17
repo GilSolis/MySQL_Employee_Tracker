3bfbaa8a05f117aff459e306e7e27be9cffcc14f
Gil Solis <gilsolisuw@gmail.com>
2020-03-16 19:33:23 -0700

    data.sql, initial sql connection

--- schema.sql
+++ schema.sql
@@ -3,13 +3,13 @@ create database initech;
 use initech;
 
 create table department (
-id INTEGER(11) NOT NULL,
+id INTEGER(11) AUTO_INCREMENT NOT NULL,
 name VARCHAR(30) NOT NULL,
 PRIMARY KEY (id)
 );
 
 create table role (
-id INTEGER(11) NOT NULL,
+id INTEGER(11) AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
 salary DECIMAL(10,2) NOT NULL,
 department_id INTEGER(11) NOT NULL,
@@ -17,7 +17,7 @@ PRIMARY KEY (id)
 );
 
 create table employees (
-id INTEGER(11) auto_increment not null,
+id INTEGER(11) AUTO_INCREMENT not null,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INTEGER(11),
--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+let mysql = require("mysql");
 
+let connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password1",
+  database: "initech"
+});
